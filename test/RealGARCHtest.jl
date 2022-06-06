75c6a7e92c19cdf10e26e61537a0221b1f6da433
Chan Kim <kchan3540@gmail.com>
2022-06-06 01:08:19 -0400

    oos model comparison

--- test/RealGARCHtest.jl
+++ test/RealGARCHtest.jl
@@ -18,28 +18,81 @@ rₜ = √hₜ zₜ,  zₜ ~ N(0,1)
 P.R. Hansen, Zhuo Huang, H.H. Shek, 2012. Realized GARCH: A joint model for returns and realized measures of volatility. Journal of applied econometrics 
 
 """
-
-
-
-
+#import package
 using CSV, DataFrames
-include("../src/ARCHModels.jl")
-using .ARCHModels
 using Optim
 using Plots
 
+include("../src/ARCHModels.jl")
+using .ARCHModels
+
+
+# read data
 readpath = dirname(pwd())*"\\ARCHModels.jl\\src\\data\\CoinbasePro_RV_ETH-USD.csv"
 df = DataFrame(CSV.File(readpath,header = 1))
 ts = df[:,1]
 rts = df[:,3]
 xts = df[:,2]
 
+#split insample / out-of-sample
+ins_n = 200
+rts_ins = rts[1:7*ins_n]
+xts_ins = xts[1:7*ins_n]
 
-spec = RealGARCH{7,1,1,1}(zeros(8+6))
-am = UnivariateARCHXModel(spec,rts,xts)
+
+
+#In-sample / out-of-sample comparison among various models  
+# in sample estimation -> out-of-sample comparison
+
+# periodic realgarch
+spec = RealGARCH{7,1,1,1}(zeros(9+6))
+am = UnivariateARCHXModel(spec,rts_ins,xts_ins)
 fitted_am = fit(am)
+fitted_coefs = fitted_am.spec.coefs
+spec = RealGARCH{7,1,1,1}(fitted_coefs)
+am = UnivariateARCHXModel(spec,rts,xts)
+ht_pregarch_os = (volatilities(am).^2)[7*ins_n+1:end]
 
-ht = volatilities(fitted_am).^2
 
-plot(ts,ht)
-plot!(xts)
+# realgarch
+spec = RealGARCH{1,1,1,1}(zeros(9))
+am = UnivariateARCHXModel(spec,rts_ins,xts_ins)
+fitted_am = fit(am)
+fitted_coefs = fitted_am.spec.coefs
+
+spec = RealGARCH{1,1,1,1}(fitted_coefs)
+am = UnivariateARCHXModel(spec,rts,xts)
+ht_regarch_os = (volatilities(am).^2)[7*ins_n+1:end]
+
+
+spec = EGARCH{1,1,1}(zeros(4))
+am = UnivariateARCHModel(spec,rts_ins)
+fitted_am = fit(am)
+fitted_coefs = fitted_am.spec.coefs
+spec = EGARCH{1,1,1}(fitted_coefs)
+am = UnivariateARCHModel(spec,rts)
+ht_egarch_os = (volatilities(am).^2)[7*ins_n+1:end]
+
+spec = TGARCH{1,1,1}(zeros(4))
+am = UnivariateARCHModel(spec,rts_ins)
+fitted_am = fit(am)
+fitted_coefs = fitted_am.spec.coefs
+spec = TGARCH{1,1,1}(fitted_coefs)
+am = UnivariateARCHModel(spec,rts)
+ht_tgarch_os = (volatilities(am).^2)[7*ins_n+1:end]
+
+
+
+σt2 =rts[7*ins_n+1:end].^2 
+σt2 =xts[7*ins_n+1:end] 
+
+mse(σt2,ht_pregarch_os), mse(σt2,ht_regarch_os),mse(σt2,ht_egarch_os),mse(σt2,ht_tgarch_os)
+mse(log.(σt2),log.(ht_pregarch_os)),mse(log.(σt2),log.(ht_regarch_os)),mse(log.(σt2),log.(ht_egarch_os)),mse(log.(σt2),log.(ht_tgarch_os))
+qlike(σt2,ht_pregarch_os), qlike(σt2,ht_regarch_os),qlike(σt2,ht_egarch_os),qlike(σt2,ht_tgarch_os)
+
+
+plot(σt2,yaxis=:log,label="RV")
+plot!(ht_pregarch_os,yaxis=:log,label="pRealGARCH",alpha=0.5)
+plot!(ht_regarch_os,label="RealGARCH",alpha=0.5)
+plot!(ht_egarch_os,label="EGARCH",alpha=0.5)
+plot!(ht_egarch_os,label="TGARCH",alpha=0.5)
